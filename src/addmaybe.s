f21125a66042ea4cd41522ace30fc96edac79407
Nicholas Mertin <nickmertin@gmail.com>
2017-08-16 02:28:45 -0400

    Added mnemonic suffixes to addmaybe.s

--- src/addmaybe.s
+++ src/addmaybe.s
@@ -3,46 +3,46 @@
     .type addmaybe, @function
 addmaybe:
     /* check if already visited */
-    bts %rsi, (%r13)
+    btsq %rsi, (%r13)
     jc addmaybe.ret
 
     /* load target character and analyze */
-    mov (%r15,%rsi), %bl
-    shl $8, %bx
-    mov 8(%rsp), %bl
-    cmp $0x23, %bh
+    movb (%r15,%rsi), %bl
+    shlw $8, %bx
+    movb 8(%rsp), %bl
+    cmpb $0x23, %bh
     je addmaybe.ret
-    cmp $0x20, %bh
+    cmpb $0x20, %bh
     je addmaybe.skip1
-    cmp $0x7A, %bh
+    cmpb $0x7A, %bh
     je addmaybe.skip1
-    cmp $0x5A, %bh
+    cmpb $0x5A, %bh
     je addmaybe.skip1
-    cmp $0x58, %bh
+    cmpb $0x58, %bh
     jne addmaybe.ret
 
     /* found the exit, set a flag */
-    or $0x80, %bl
+    orb $0x80, %bl
 
     /* normal target; add new node */
 addmaybe.skip1:
-    mov %r11, (%rbp)
-    mov %esi, 8(%rbp)
-    movzb %bl, %edi
-    mov %edi, 12(%rbp)
+    movq %r11, (%rbp)
+    movl %esi, 8(%rbp)
+    movzbl %bl, %edi
+    movl %edi, 12(%rbp)
 
     /* check if exit flag set */
-    test %bl, %bl
+    testb %bl, %bl
     jns addmaybe.skip2
 
     /* this is the exit; remove exit flag from node and print results */
-    mov %rbp, %r11
-    and $0x7F, 12(%rbp)
-    mov 56(%rsp), %rbp
+    movq %rbp, %r11
+    andq $0x7F, 12(%rbp)
+    movq 56(%rsp), %rbp
     jmp result
 addmaybe.skip2:
 
     /* push lead and return */
-    call pushlead
+    callq pushlead
 addmaybe.ret:
-    ret
+    retq
