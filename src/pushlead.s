bd80af93547e52e37f16c2fb1e5df61aaa47ef5d
Nicholas Mertin <nickmertin@gmail.com>
2017-08-16 02:44:13 -0400

    Added mnemonic suffixes to pushlead.s

--- src/pushlead.s
+++ src/pushlead.s
@@ -3,12 +3,12 @@
     .type pushlead, @function
 pushlead:
     /* add lead to lead buffer */
-    mov %r10, -8(%rsp)
-    mov 40(%rsp), %r10
-    mov %rbp, (%r10)
-    lea 8(%r10), %r10
+    movq %r10, -8(%rsp)
+    movq 40(%rsp), %r10
+    movq %rbp, (%r10)
+    leaq 8(%r10), %r10
     movq $0, (%r10)
-    mov %r10, 40(%rsp)
-    lea 16(%rbp), %rbp
-    mov -8(%rsp), %r10
-    ret
+    movq %r10, 40(%rsp)
+    leaq 16(%rbp), %rbp
+    movq -8(%rsp), %r10
+    retq
